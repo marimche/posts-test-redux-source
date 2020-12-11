2d88acc91a701f6b9a25bc11899e9f426a0090f5
March <mch@mail.com>
2020-12-11 10:33:28 +0400

    add header

--- src/App.js
+++ src/App.js
@@ -10,21 +10,25 @@ function App() {
       <Header 
         title="Блог"
         description="для тестирования redux"
-        imageUrl="https://images.unsplash.com/photo-1607453815112-423fc171d5ea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+        imageUrl="https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
       />
-      <div className="row">
-        <div className="col">
-          <PostForm />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <h3>Созданные посты (синхронно)</h3>
-          <Posts />
-        </div>
-        <div className="col">
-          <h3>Загруженные посты (асинхронно)</h3>
-          <FetchedPosts />
+      <div className="container">
+        <div className="content">
+          <div className="row">
+            <div className="col">
+              <PostForm />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h3>Созданные посты (синхронно)</h3>
+              <Posts />
+            </div>
+            <div className="col">
+              <h3>Загруженные посты (асинхронно)</h3>
+              <FetchedPosts />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import classes from './Header.module.css'
 
-export default ({title, description, imageUrl}) => {
+	export default ({title, description, imageUrl}) => {
 	return (
+		// <div className={classes.headerBlock} style={{ backgroundImage: 'url(${imageUrl})'}}>
 		<div className={classes.headerBlock}>
 			<div className="container">
-				<div className="header-block__overlay"></div>
-				<div className="header-block__center">
+				<div className={classes.headerBlock__overlay}></div>
+				<div className={classes.headerBlock__center}>
 					<h1>{title}</h1>
 					<h3>{description}</h3>
 				</div>
