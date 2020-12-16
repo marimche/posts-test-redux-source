356e4a77010102f7393f2f28b5d3710d5fdef0de
March <mch@mail.com>
2020-12-16 15:11:32 +0400

    add routing

--- src/components/AddFormPost/AddFormPost.js
+++ src/components/AddFormPost/AddFormPost.js
@@ -6,19 +6,19 @@ const AddFormPost = () => {
 		<form className="add-form">
 		<div className="add-form__row">
 		  <h5>
-			 <label for="title" class="form-label">Заголовок</label>
+			 <label htmlFor ="title" className="form-label">Заголовок</label>
 		  </h5>
 		  <input type="text" className="form-control" id="title" aria-describedby="title"/>
 		</div>
 		<div >
 		  <h5>
-			 <label for="image" class="form-label">URL Картинки</label>
+			 <label htmlFor ="image" className="form-label">URL Картинки</label>
 		  </h5>
 		  <input type="text" className="form-control" id="image" aria-describedby="image"/>
 		</div>
 		<div >
 		  <h5>
-			 <label for="description" class="form-label">Описание</label>
+			 <label htmlFor ="description" className="form-label">Описание</label>
 		  </h5>
 		  <textarea rows="8" className="form-control" id="description" aria-describedby="description"/>
 		</div>
--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import classes from './PostItem.module.css';
 
 const PostItem = ({title, createAt, id}) => (
 		<div className={classes.postItem}>
-			<a href={'/post/${_id}'}>
+			<Link to={'/post/${_id}'}>
 				<h2>{title}</h2>
-			</a>
+			</Link>
 			<p>
 				<i>Дата публикации: {createAt}</i>
-				<a href='#'>Удалить</a>
-				<a href='#'>Редактировать</a>
+				<Link to='#'>Удалить</Link>
+				<Link to='#'>Редактировать</Link>
 			</p>
       </div>
 	);
