6436cd36aa67839170e751345939fa63d0a8baca
March <mch@mail.com>
2021-01-08 18:36:53 +0300

    returning the interface

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="container pt-3">
-      <Header 
+      <Header  
         title="Блог"
         description="для тестирования redux"
         imageUrl="https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
