111f976581678d4001788e3a3e425ed198178b74
March <mch@mail.com>
2021-01-08 19:05:32 +0300

    Updates

--- src/components/Loader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Loader = () => (
-	<div className="spinner-border text-primary" role="status">
-  		<span className="sr-only">Loading...</span>
-	</div>
-)
